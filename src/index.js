a93b109e39ded9955bc67aa5d5fb24327f88f222
CSGerardo <t.carlos.cg@gmail.com>
2024-06-03 20:17:04 -0700

    Created Store and Provider in index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import "./index.css";
 import App from "./Containers/App";
 import reportWebVitals from "./reportWebVitals";
+import { searchRobots } from "./reducers.js";
 import "tachyons";
+
+const store=createStore(searchRobots);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>  
+    <App store={store} />  
   </React.StrictMode>
 );
 
