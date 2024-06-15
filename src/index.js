e5c25bb1a7740bb1ccb4bc469276140fa9d478bd
CSGerardo <t.carlos.cg@gmail.com>
2024-06-15 13:58:17 -0700

    Installed redux-logger to add a middleware, wrapped App container in Provider component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
 import "./index.css";
 import App from "./Containers/App";
 import reportWebVitals from "./reportWebVitals";
 import { searchRobots } from "./reducers.js";
 import "tachyons";
 
-const store=createStore(searchRobots);
+const logger=createLogger();
+const store=createStore(searchRobots, applyMiddleware(logger));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App store={store} />  
+    <Provider store={store}>
+      <App/> 
+    </Provider> 
   </React.StrictMode>
 );
 
